633ef44fa97f39813ffe546602ba3fde5b517ead
Lucas Nery <lucas.leite1@br.nestle.com>
2024-12-30 14:11:38 -0300

    update utils

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -23,3 +23,21 @@ export const years = [
 	2027,
 	2028,
 ]
+
+export const parseCurrencyString = (currencyString: string) => {
+	let numberString = currencyString.replace("R$", "").trim(); // Remove the currency symbol (e.g., "R$") and trim whitespace
+	numberString = numberString.replace(/\./g, ""); // // Remove thousand separators (.)
+	numberString = numberString.replace(",", "."); // // Replace the decimal separator (,) with a dot (.)
+	
+	return parseFloat(numberString); // Convert the cleaned string to a float
+}
+
+export const formatToCurrencyBRL = (amount: number) => {
+	// Format the number to Brazilian Real currency
+	return amount.toLocaleString('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2
+	});
+}
