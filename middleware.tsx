973c80c65a299f1d200a847f77784035d61ed81f
Lucas nery <lucasnerytoledo@hotmail.com>
2025-02-25 08:55:20 -0300

    update domain

--- middleware.tsx
+++ middleware.tsx
@@ -2,32 +2,16 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-let lastToken = ""
-
 export async function middleware(request: NextRequest) {
   const cookieStore = await cookies();
   const token = cookieStore.get('access_token');
 
-  if (!token && !lastToken) {
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/token`, { method: "GET" });
-      const data = await res.json();
-      const newToken = data.token;
-
-      if (newToken.message || newToken === undefined || !newToken.length) {
-        throw new Error();
-      }
-
-      lastToken = newToken
-      return NextResponse.next();
-    } catch (error) {
-      console.log("Erro ao buscar o token:", error);
-      cookieStore.delete("access_token")
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  if (token) {
+    return NextResponse.next();
   }
 
-  return NextResponse.next();
+  return NextResponse.redirect(new URL('/login', request.url));
+
 }
 
 export const config = {
