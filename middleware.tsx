baa3e22b450ec4ce2f0fc87a08c720f35bbdcf0d
Lucas nery <lucasnerytoledo@hotmail.com>
2025-03-19 15:16:17 -0300

    update middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,30 +1,7 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-const publicRoutes = ["/login", "/register"]
-
-const REDIRECT_URL = "/login"
-
-export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname
-  const publicRoute = publicRoutes.find(route => route === path)
-  const authToken = request.cookies.get("access_token")
-
+export async function middleware() {
   return NextResponse.next();
-
-  if (!authToken && publicRoute) {
-    return NextResponse.next();
-  }
-
-  if (!authToken && !publicRoute) {
-    const redirectUrl = request.nextUrl.clone()
-    redirectUrl.pathname = REDIRECT_URL
-
-    return NextResponse.redirect(redirectUrl);
-  }
-
-  if (authToken && !publicRoute) {
-  }
 }
 
 export const config = {
